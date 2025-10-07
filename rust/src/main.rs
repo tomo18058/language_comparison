2c3511c96b4baaf3e7de760598d04e4e0a8e3174
原知輝 <tomontomon1203@icloud.com>
2025-10-07 17:04:11 +0900

    Rust コード修正・動作確認

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,14 +1,5 @@
-use std::time::Instant;
-
 fn main() {
-    let start = Instant::now();
 
-    let mut sum: i64 = 0;
-    for i in 0..100_000_000 {
-        sum += i;
-    }
+    println!("Hello World");
 
-    let duration = start.elapsed();
-    println!("Rust sum = {}", sum);
-    println!("Rust time: {:.4} sec", duration.as_secs_f64());
 }
